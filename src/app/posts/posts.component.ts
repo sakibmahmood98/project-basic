06f2a0c5451a3fe7035bfd4bad72457c5e22902a
sakibmahmood09 <sakibmahmood09@gmail.com>
2021-09-03 23:39:31 +0600

    Creating data using dummy api link

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'posts',
@@ -8,12 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent  {
    public posts!: any[];
+   private url = 'https://jsonplaceholder.typicode.com/posts';
 
   constructor(private http: HttpClient) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
+    http.get(this.url)
     .subscribe(response => {
       this.posts = response as any;
      
+    });
+   }
+
+   createPost(input: HTMLInputElement) {
+     let post :any = {title: input.value };
+     input.value = '';
+
+
+    this.http.post(this.url,JSON.stringify(post) )
+    .subscribe(response => {
+      post.id = response;
+      this.posts.splice(0,0,post);
+    
     });
    }
 }
