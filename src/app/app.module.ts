8f7905c2caba8a6b61fb7f87ab78cd2f30cc1ffd
sakibmahmood09 <sakibmahmood09@gmail.com>
2021-09-06 12:17:21 +0600

    Accessing Route Parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -66,7 +66,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     RouterModule.forRoot([
 
       { path: ' ', component: HomeComponent},
-      { path: 'profile/":username', component: GithubProfileComponent},
+      { path: 'followers/:username', component: GithubProfileComponent},
       { path: 'followers', component: GithubFollowersComponent },
       { path: 'posts', component: PostsComponent},
       { path: '**', component: NotFoundComponent}
--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
 
+  //  this.route.paramMap['username']
+
+    this.route.paramMap
+    .subscribe(params => {
+      let id = params.get('username');
+      console.log(id);
+    });
+  }
 }
