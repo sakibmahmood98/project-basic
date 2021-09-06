a98c7634c4736340123e8edd64a91ba7f7e74ddd
sakibmahmood09 <sakibmahmood09@gmail.com>
2021-09-07 02:30:08 +0600

    Added logout() using fake backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
-  selector: 'app-home',
+  selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  archives = [
-    { year: 2021, month: 9},
-    { year: 2021, month: 10},
-    { year: 2021, month: 11},
-    { year: 2021, month: 12},
+export class HomeComponent {
+  // archives = [
+  //   { year: 2021, month: 9},
+  //   { year: 2021, month: 10},
+  //   { year: 2021, month: 11},
+  //   { year: 2021, month: 12},
 
-  ]
+  // ]
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(public authService: AuthService) { }
 
 }
